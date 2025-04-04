93be5dc44a420cdcfac2ddd6077908a2ccd83d0c
thanhntse <nguyenthaithanh07@gmail.com>
2025-04-05 01:35:36 +0700

    feat: add mvc project

---
+++ PRN222.Assignment.FUHotelBookingSystem.MVC/Controllers/HotelManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN222.Assignment.FUHotelBookingSystem.MVC.Controllers
+{
+    public class HotelManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PRN222.Assignment.FUHotelBookingSystem.MVC/Controllers/OverviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN222.Assignment.FUHotelBookingSystem.MVC.Controllers
+{
+    public class OverviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
